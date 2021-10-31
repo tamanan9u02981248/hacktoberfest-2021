99c46c4858bc38ae62e3c7ef8bb569d4a6a2fbd4
Jefferson Silva <e_jeffersonsilva@jfrn.jus.br>
2021-10-31 18:03:28 -0300

    inversão de string

--- hacktoberfest2021.py
+++ hacktoberfest2021.py
@@ -1,8 +1,15 @@
 escolha = 1
 
+def inverter_string():
+    string = str(input("Digite a string para inverter: "))
+    string = string[::-1].upper()
+    print("resultado: {} \n".format(string))
+
 def menu():
     global escolha
-    escolha = int(input("0 - sair \n\n"))
+    escolha = int(input("1 - Inverter String \n0 - sair \n\n"))
+    if (escolha == 1):
+        inverter_string()
 
 while (escolha != 0):
     menu()
