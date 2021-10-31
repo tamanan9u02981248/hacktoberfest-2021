4fb432c6c3f84e475d47318bde46aea8becba545
Jefferson Silva <e_jeffersonsilva@jfrn.jus.br>
2021-10-31 18:34:43 -0300

    embalhar string

--- hacktoberfest2021.py
+++ hacktoberfest2021.py
@@ -1,3 +1,5 @@
+import random
+
 escolha = 1
 
 def inverter_string():
@@ -11,13 +13,21 @@ def string_escada():
         print(" ".join(list(string[0:index].upper())))
     print('\n')
 
+def string_embaralhada():
+    string = list(str(input("Digite a string para embaralhar: ")))
+    random.shuffle(string)
+    print("".join(string))
+
 def menu():
     global escolha
-    escolha = int(input("1 - Inverter String \n2 - String em Escada \n0 - sair \n\n"))
+    escolha = int(input("1 - Inverter String \n2 - String em Escada \n3 - Embaralhar string \n0 - sair \n\n"))
+
     if (escolha == 1):
         inverter_string()
     elif (escolha == 2):
         string_escada()
+    elif (escolha == 3):
+        string_embaralhada()
 
 while (escolha != 0):
     menu()
